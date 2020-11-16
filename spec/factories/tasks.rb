806ab67d60071b6986a62e6e8344fb86e39b1d6f
noriko ogihara <aloha.nori1104@gmail.com>
2020-11-17 08:23:43 +0900

    Add Rspec System Test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,25 +3,30 @@ FactoryBot.define do
     name { 'title1' }
     description { 'content1' }
     created_at { Time.current + 1.days }
+    expired_at { Time.current + 1.days }
   end
   factory :second_task, class: Task do
     name { 'title2' }
     description { 'content2' }
     created_at { Time.current + 2.days }
+    expired_at { Time.current + 2.days }
   end
   factory :third_task, class: Task do
     name { 'title3' }
     description { 'content3' }
     created_at { Time.current + 3.days }
+    expired_at { Time.current + 3.days }
   end
   factory :fourth_task, class: Task do
     name { 'title4' }
     description { 'content4' }
     created_at { Time.current + 4.days }
+    expired_at { Time.current + 4.days }
   end
   factory :fifth_task, class: Task do
     name { 'title5' }
     description { 'content5' }
     created_at { Time.current + 5.days }
+    expired_at { Time.current + 5.days }
   end
 end
--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -11,6 +11,7 @@ describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'task_name', with: task.name
         fill_in 'task_description', with: task.description
+        fill_in 'task_expired_at', with: task.expired_at
         click_on 'commit'
         expect(page).to have_content 'task'
       end
@@ -28,6 +29,15 @@ describe 'タスク管理機能', type: :system do
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
         visit tasks_path
+        task_list = all('.task_name')
+        expect(task_list[0]).to have_content 'title5'
+      end
+    end
+
+    context '「終了期限でソートする」というボタンを押した場合' do
+      it '終了期限の降順に並び替えられる' do
+        visit tasks_path
+        click_on "終了期限でソートする"
         task_list = all('.task_name')
         expect(task_list[0]).to have_content 'title5'
       end
