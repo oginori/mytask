3dea3d7de10525a615e44cf62af10a67c91fe3de
noriko ogihara <aloha.nori1104@gmail.com>
2020-12-13 18:51:06 +0900

    Fix seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-User.create!(name: "test", email: "test@example.com",password: "password", admin: true )
-Task.create!(name: "sample", description: "sample_description", user_id: 1)
-5.times do |i|
-    Label.create!(name: "sample#{i + 1}")
-  end
+User.create!(name: "admin", email: "admin@example.com", password: "admin_password", admin: true )
+
+10.times do |i|
+  User.create!(name: "test#{i + 1}", email: "test#{i + 1}@example.com",password: "password#{i + 1}", admin: false )
+  Task.create!(name: "sample#{i + 1}", description: "sample#{i + 1}_description", user_id: "#{i + 1}" )
+  Label.create!(name: "sample#{i + 1}")
+end
