5a5c9ee6b7a34d73fbd7f232228ff1ab1f8aa138
noriko ogihara <aloha.nori1104@gmail.com>
2020-12-20 16:12:38 +0900

    Fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,16 @@ User.create!(name: "admin", email: "admin@example.com", password: "admin_passwor
   Labelling.create!(task_id: "#{i + 1}".to_i, label_id: "#{i + 1}")
 end
 
-Task.create!(name: "sample11", description: "sample11_description", user_id: 1 )
+Task.create!(name: "sample11", description: "sample11_description", user_id: 1)
 Task.create!(name: "sample12", description: "sample12_description", user_id: 1)
 Task.create!(name: "sample15", description: "sample15_description", user_id: 5)
 
 3.times do |i|
-  Labelling.create!(task_id: "#{i + 1}", label_id: 1)
+  Labelling.create!(task_id: "#{i + 1}", label_id: 3)
   Labelling.create(task_id: 5, label_id: "#{i + 2}".to_i )
 end
+
+Labelling.create(task_id: 11, label_id: 6)
+Labelling.create(task_id: 11, label_id: 10)
+Labelling.create(task_id: 12, label_id: 8)
+Labelling.create(task_id: 13, label_id: 4)
