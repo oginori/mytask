32a984f33923d5e3e38c432b4883ccf52fad9f67
noriko ogihara <aloha.nori1104@gmail.com>
2020-12-20 14:50:00 +0900

    Fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create!(name: "admin", email: "admin@example.com", password: "admin_passwor
 
 10.times do |i|
   User.create!(name: "test#{i + 1}", email: "test#{i + 1}@example.com",password: "password#{i + 1}", admin: false )
-  Task.create!(name: "sample#{i + 1}", description: "sample#{i + 1}_description", user_id: "#{i + 1}" )
+  Task.create!(name: "sample#{i + 1}", description: "sample#{i + 1}_description", user_id: "#{i + 1}".to_i )
   Label.create!(name: "sample#{i + 1}")
 end
 
